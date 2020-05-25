8c505364e41aef20c1913424384dbf7c3f54f388
Suyash Saurabh <suyashsaurabh10@gmail.com>
2020-05-25 21:37:44 +0530

    initial commit, Added REST API for ITEM USER authentication and stores

---
+++ resources/user.py
@@ -0,0 +1,31 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from models.user import UserModel
+
+class UserRegister(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'username',
+        type = str,
+        required = True,
+        help ="This field Is Required"
+    )
+
+    parser.add_argument(
+        'password',
+        type = str,
+        required = True,
+        help ="This field Is Required"
+    )
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        if UserModel.find_by_username(data['username']):
+            return {"message":"A ser with that username already exists"}, 400
+
+        user = UserModel(**data)
+        user.save_to_db()
+
+        return {"message":"User created successfully."}, 201
